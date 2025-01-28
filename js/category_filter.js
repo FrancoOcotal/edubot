1cc1dceff7fcbd8fe42828c066430220f3f86d08
FrancoOcotal <154026276+FrancoOcotal@users.noreply.github.com>
2025-01-28 16:50:45 -0600

    Update category_filter.js

--- js/category_filter.js
+++ js/category_filter.js
@@ -49,7 +49,7 @@ function renderPosts(posts, clearExisting = false) {
         if (image) {
             imageEmbed = `
                 <div class="mt-4">
-                    <img src="${image}" alt="Imagen del post" class="w-32 rounded-lg shadow-md">
+                    <img src="${image}" alt="Imagen del post" class="w-30 rounded-lg shadow-md">
                 </div>
             `;
         }
