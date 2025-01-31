fa3ff60dc116f88251700b50d849af430c9f29f7
FrancoOcotal <154026276+FrancoOcotal@users.noreply.github.com>
2025-01-31 07:26:12 -0600

    Add files via upload

--- games/animals/script.js
+++ games/animals/script.js
@@ -25,15 +25,7 @@ gameOverMessage.style.textAlign = 'center';
 gameOverMessage.style.animation = 'fadeIn 2s ease-in-out';
 document.body.appendChild(gameOverMessage);
 
-const questions = [
-    { image: 'dog.png', question: 'What is this word?', answers: ['Dog', 'Cat', 'Rabbit'], correct: 'Dog' },
-    { image: 'cat.png', question: 'What is this word?', answers: ['Rabbit', 'Cat', 'Dog'], correct: 'Cat' },
-    { image: 'rabbit.png', question: 'What is this word?', answers: ['Cat', 'Rabbit', 'Dog'], correct: 'Rabbit' },
-    { image: 'ostrich.png', question: 'What is this word?', answers: ['Ostrich', 'Elephant', 'Owl'], correct: 'Ostrich' },
-    { image: 'owl.png', question: 'What is this word?', answers: ['Elephant', 'Owl', 'Ostrich'], correct: 'Owl' },
-    { image: 'elephant.png', question: 'What is this word?', answers: ['Dog', 'Elephant', 'Rabbit'], correct: 'Elephant' }
-];
-
+// Inicialización
 let shuffledQuestions = [];
 let currentQuestionIndex = 0;
 let score = 0;
