0fde91260b7c3b898c9863227fb35301580b480c
FrancoOcotal <154026276+FrancoOcotal@users.noreply.github.com>
2025-02-15 17:20:25 -0600

    Add files via upload

---
+++ games/family/questions.js
@@ -0,0 +1,8 @@
+const questions = [
+    { image: 'dog.png', question: 'What is this word?', answers: ['Dog', 'Cat', 'Rabbit'], correct: 'Dog' },
+    { image: 'cat.png', question: 'What is this word?', answers: ['Rabbit', 'Cat', 'Dog'], correct: 'Cat' },
+    { image: 'rabbit.png', question: 'What is this word?', answers: ['Cat', 'Rabbit', 'Dog'], correct: 'Rabbit' },
+    { image: 'ostrich.png', question: 'What is this word?', answers: ['Ostrich', 'Elephant', 'Owl'], correct: 'Ostrich' },
+    { image: 'owl.png', question: 'What is this word?', answers: ['Elephant', 'Owl', 'Ostrich'], correct: 'Owl' },
+    { image: 'elephant.png', question: 'What is this word?', answers: ['Dog', 'Elephant', 'Rabbit'], correct: 'Elephant' }
+];
